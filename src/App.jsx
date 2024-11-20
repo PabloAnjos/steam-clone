00e20f0f44aaaba3510fe2613d74091c5117902c
Pablo Anjos <pablomatheussilvaanjoss@gmail.com>
2024-11-20 15:21:24 -0300

    Header finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import logoSteam from './assets/images/logo-steam.png'
 import avatar from './assets/images/avatar.png'
-import arrow from './assets/images/arrow.png'
-import message from './assets/images/message.png'
-import noticiations from './assets/images/noticiations.png'
+import { Bell, ChatCircle, CaretDown } from '@phosphor-icons/react'
 
 function App() {
   return (
     <>
         <header>
-                <nav>
+                <nav className='flex justify-between items-center w-screen h-20 bg-[#1B2838] pr-8 pl-8'>
                       <div>
-                          <ul>
+                          <ul className='flex items-center gap-9 text-white'>
                               <li><img src={logoSteam} alt="Logo Steam" /></li>
-                              <li>LOJA</li>
-                              <li>BIBLIOTECA</li>
-                              <li>COMUNIDADE</li>
-                              <li>AJUDA</li>
+                              <li className='hover:text-[#0094FF] cursor-pointer'>LOJA</li>
+                              <li className='hover:text-[#0094FF] cursor-pointer'>BIBLIOTECA</li>
+                              <li className='hover:text-[#0094FF] cursor-pointer'>COMUNIDADE</li>
+                              <li className='hover:text-[#0094FF] cursor-pointer'>AJUDA</li>
                           </ul>
                       </div>
 
                       <div>
-                            <ul>
-                              <li><img src={noticiations} alt="Icone noticiations" /></li>
-                              <li><img src={message} alt="Icone message" /></li>
-                              <li><img src={avatar} alt="Imagem avatar" /></li>
+                            <ul className='flex items-center gap-8'>
+                              <Bell size={25} color='white' className='cursor-pointer'/>
+                              <ChatCircle size={25} color='white'  className='cursor-pointer'/>
+                              <li><img src={avatar} alt="Imagem avatar"  className='cursor-pointer'/></li>
+                              <CaretDown size={18} color='#0094FF'  className='cursor-pointer'/>
                             </ul>
                       </div>
                 </nav>
